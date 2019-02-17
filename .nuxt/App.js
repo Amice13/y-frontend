8e7b780d76024bdb666d3498623be42d1bd756a0
Amice13 <kirillzakharov13@gmail.com>
2019-02-17 22:53:59 +0200

    Added list of images with actions

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -8,7 +8,7 @@ import _6f6c098b from '../layouts/default.vue'
 const layouts = { "_default": _6f6c098b }
 
 export default {
-  head: {"title":"tooltip_image_manager","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"Yalantis Tooltip Image Manager"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
+  head: {"title":"tooltip_image_manager","mode":"spa","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"Yalantis Tooltip Image Manager"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
 
   render(h, props) {
     const loadingEl = h('NuxtLoading', { ref: 'loading' })
--- pages/index.vue
+++ pages/index.vue
@@ -4,26 +4,58 @@
     <!-- Main page -->
     <v-flex xs12>
       <v-card>
-        <v-card-title class="headline">Tooltip Picture Manager</v-card-title>
+        <v-card-title class="display-2">Tooltip Picture Manager</v-card-title>
         <v-card-text>
           <input type="file" hidden accept=".png, .jpg, .jpeg, .svg, .gif" id="file" ref="file" @change="handleFileUpload">
-          <v-layout row wrap>
-            <v-flex xs10>
-              <!-- Add filtering options -->
+          <v-layout row wrap pb-3>
+            <v-flex xs10 sm9 id="search-place">
+              <v-text-field name="search" v-model="filter" @keyup.enter="filterList" solo hide-details prepend-icon="search" label="Search" id="search"></v-text-field>
             </v-flex>
-            <v-flex xs2 text-xs-right>
-              <v-btn @click.native.prevent="$refs.file.click" color="primary">
+            <v-flex xs12 sm3 text-xs-right>
+              <v-btn block @click.native.prevent="$refs.file.click" color="primary">
                 <v-icon left>publish</v-icon> Upload 
               </v-btn>
             </v-flex>
           </v-layout>
           <v-divider></v-divider>
-          <v-layout row wrap pt-4 pb-1>
+          <v-layout row wrap pt-4 pb-1 v-if="imageList.length === 0">
             <v-flex xs12 text-xs-center>
               You have not uploaded any images yet!
-            </v-flex>
-            
+            </v-flex>            
           </v-layout>
+          <div v-if="imageList.length > 0">
+            <v-layout row wrap pt-4 pb-1 hidden-sm-and-down class="grey lighten-3">
+              <v-flex xs1 pl-2>Preview</v-flex>
+              <v-flex xs4 pl-2 pr-2 @click="sortList('tooltip')" class="sortable">Tooltip</v-flex>
+              <v-flex xs2 pl-2 pr-2 @click="sortList('name')" class="sortable">Filename</v-flex>
+              <v-flex xs2 pl-2 pr-2 text-xs-center>Properties</v-flex>
+              <v-flex xs1 pl-2 pr-2 text-xs-center @click="sortList('size')" class="sortable">Size</v-flex>
+              <v-flex xs2 pl-2 pr-2 text-xs-center>Actions</v-flex>
+            </v-layout>
+            <v-layout v-for="(item, index) in imageList" align-center :key="'image-' + index" row wrap pb-1 style="border-bottom:1px solid #ccc;">
+              <v-flex sm1 xs12 pl-2 pr-2>
+                <v-img :src="item.source" :aspect-ratio="1" />
+              </v-flex>
+              <v-flex sm4 pb-2 xs12 pl-2 pr-2 text-md-left text-xs-center>{{ item.tooltip }}</v-flex>
+              <v-flex sm2 xs4 pl-2 pr-2 text-md-left text-xs-center>{{ item.name }}</v-flex>
+              <v-flex sm2 xs4 pl-2 pr-2 text-xs-center>{{ item.width }} × {{ item.height }}</v-flex>
+              <v-flex sm1 xs4 pl-2 pr-2 text-xs-center>{{ (item.size / (Math.pow(1024, 2))).toFixed(2) }} MB</v-flex>
+              <v-flex sm2 xs12 pl-2 pr-2 text-xs-center>
+                <v-btn small :block="$vuetify.breakpoint.smAndDown" :flat="!$vuetify.breakpoint.smAndDown">
+                  <v-icon :left="$vuetify.breakpoint.smAndDown">play_arrow</v-icon>
+                  <span class="hidden-sm-and-up"> View</span></v-btn>
+                <v-btn small :block="$vuetify.breakpoint.smAndDown" :flat="!$vuetify.breakpoint.smAndDown">
+                  <v-icon :left="$vuetify.breakpoint.smAndDown">edit</v-icon>
+                  <span class="hidden-sm-and-up"> Edit</span></v-btn>
+                <v-btn small :block="$vuetify.breakpoint.smAndDown" :flat="!$vuetify.breakpoint.smAndDown">
+                  <v-icon :left="$vuetify.breakpoint.smAndDown">delete</v-icon>
+                  <span class="hidden-sm-and-up"> Delete</span></v-btn>
+              </v-flex>
+            </v-layout>
+          </div>
+          <div class="text-xs-center">
+            <v-pagination v-show="pages > 1" v-model="page" :length="pages"></v-pagination>
+          </div>
         </v-card-text>
       </v-card>
     </v-flex>
@@ -44,7 +76,7 @@
             <v-flex xs9 pl-4>
               <p><strong>Filename:</strong> {{ image.name }}</p>
               <p><strong>Size:</strong> {{ (image.size / (Math.pow(1024, 2))).toFixed(2) }} MB</p>
-              <p><strong>Width and height</strong> {{ image.width }}x{{ image.height }}</p>
+              <p><strong>Width and height</strong> {{ image.width }} × {{ image.height }}</p>
               <p><strong>Uploaded at:</strong> {{ image.createdAt.toLocaleString('ru', { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' }) }}</p>
               <v-text-field name="tooltip" label="Add tooltip" v-model="image.tooltip"></v-text-field>
             </v-flex>
@@ -97,16 +129,13 @@ import Nedb from 'nedb'
 const db = new Nedb({ filename: 'images', autoload: true })
 
 export default {
-  mounted () {
+  async mounted () {
     let image = this.$refs.image
-    db.find({}, (error, data) => {
-      if (error) this.error = error
-      this.imageList = data
-    })
     image.onload = () => {
       this.image.width = image.naturalWidth
       this.image.height = image.naturalHeight
     }
+    this.filterList()
   },
   methods: {
     async handleFileUpload () {
@@ -130,6 +159,51 @@ export default {
         this.uploadDialog = false
         return null
       })
+    },
+    filterList () {
+      this.page = 1
+      let query = { $or: [
+        { name: { $regex: new RegExp(this.filter, 'gi') } },
+        { tooltip: { $regex: new RegExp(this.filter, 'gi') } }
+      ] }
+      this.cursor = db.find(query).sort({ [this.sort]: this.sortAsc })
+      this.cursor.limit(this.limit).exec((error, data) => {
+        if (error) {
+          this.error = error
+          return null
+        }
+        this.imageList = data
+      })
+      db.count(query, (error, count) => {
+        if (error) this.error = error
+        this.pages = Math.ceil(count / this.limit)
+      })
+      return false
+    },
+    sortList (type) {
+      if (this.sort === type) {
+        this.sortAsc = this.sortAsc === 1 ? -1 : 1
+      } else {
+        this.sort = type
+        this.sortAsc = 1
+      }
+      this.filterList()
+    }
+  },
+  computed: {
+    skip () {
+      return (this.page - 1) * this.limit
+    }
+  },
+  watch: {
+    page: function (value) {
+      this.cursor.skip(this.skip).exec((error, data) => {
+        if (error) {
+          this.error = error
+          return null
+        }
+        this.imageList = data
+      })
     }
   },
   data () {
@@ -137,6 +211,10 @@ export default {
       file: '',
       imageSource: '',
       imageList: [],
+      dbCursor: null,
+      filter: '',
+      sort: 'createdAt',
+      sortAsc: 1,
       image: {
         source: '',
         height: '',
@@ -148,7 +226,10 @@ export default {
       },
       error: '',
       success: '',
-      uploadDialog: false
+      uploadDialog: false,
+      page: 1,
+      pages: 0,
+      limit: 10
     }
   }
 }
@@ -163,3 +244,21 @@ const getDataUrl = (file) => {
   })
 }
 </script>
+
+<style>
+
+#search-place .v-input__control {
+  min-height: 36px !important;
+  padding: 6px !important;
+}
+
+.sortable {
+  cursor: pointer;
+  font-weight: bold;
+}
+
+.sortable:hover {
+  text-decoration: underline;
+}
+
+</style>
